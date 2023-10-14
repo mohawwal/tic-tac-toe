75f11eaf0cdab066a45f22ea8034610f87195fcc
Anileleye Awwal <aanileleye@gmail.com>
2023-10-15 00:07:06 +0100

    displayed game moves

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -23,7 +23,7 @@ export default function Game({ toggleMode, isDark }) {
     const moves = history.map((squares, move) => {
         let description;
         if (move > 0) {
-          description = 'Go to move #' + move;
+          description = '#' +  move + " move";
         } else {
           description= 'Go to game start';
         }
