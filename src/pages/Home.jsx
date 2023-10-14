883a0d0ccee11dc29e176c4832143949f0e9c77d
Anileleye Awwal <aanileleye@gmail.com>
2023-10-14 23:36:06 +0100

    completed style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>TIC TAC TOE</h1>
       <img src="/images/tic-tac-toe-img.jpg" alt="" />
       <Link to="/Game" >
